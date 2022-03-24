21078c16de8e3881a5ab8a191bf7414c0f739855
Sergio Santiago <sergiosantiagop@gmail.com>
2022-03-24 13:09:04 +0100

    Init project

---
+++ src/main/main.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import "./main.scss";
+
+const Main = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    setLoading(true);
+    // fetch
+    // setData
+    setLoading(false);
+  };
+
+  return (
+    <div className="main">
+      <div className="main__following"></div>
+      <div className="main__follow"></div>
+      <div className="main__timeline"></div>
+      <div className="main__message"></div>
+    </div>
+  );
+};
+export default Main;
