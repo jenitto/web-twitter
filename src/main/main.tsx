1729d29eed25a23f4fb3b935e888fde8f55aa0a5
Sergio Santiago <sergiosantiagop@gmail.com>
2022-03-24 18:55:58 +0100

    Add linter

--- src/main/main.tsx
+++ src/main/main.tsx
@@ -1,28 +1,28 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import "./main.scss";
 
-const Main = () => {
-  const [loading, setLoading] = useState<boolean>(true);
-  const [data, setData] = useState([]);
+function Main() {
+  // const [loading, setLoading] = useState<boolean>(true);
+  // const [data, setData] = useState([]);
 
-  useEffect(() => {
-    getData();
-  }, []);
+  // useEffect(() => {
+  //   getData();
+  // }, []);
 
-  const getData = async () => {
-    setLoading(true);
-    // fetch
-    // setData
-    setLoading(false);
-  };
+  // const getData = async () => {
+  //   setLoading(true);
+  //   // fetch
+  //   // setData
+  //   setLoading(false);
+  // };
 
   return (
     <div className="main">
-      <div className="main__following"></div>
-      <div className="main__follow"></div>
-      <div className="main__timeline"></div>
-      <div className="main__message"></div>
+      <div className="main__following" />
+      <div className="main__follow" />
+      <div className="main__timeline" />
+      <div className="main__message" />
     </div>
   );
-};
+}
 export default Main;
