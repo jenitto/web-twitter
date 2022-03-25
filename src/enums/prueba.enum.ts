cf6600a87841f9c60789cdb22668ee1d05cb4f87
Sergio Santiago <sergiosantiagop@gmail.com>
2022-03-25 23:03:13 +0100

    Remove linter

---
+++ src/enums/prueba.enum.ts
@@ -0,0 +1,3 @@
+export interface Hola {
+  title: string;
+}
--- src/main/main.tsx
+++ src/main/main.tsx
@@ -1,27 +1,27 @@
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./main.scss";
 
 function Main() {
-  // const [loading, setLoading] = useState<boolean>(true);
-  // const [data, setData] = useState([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [data, setData] = useState([]);
 
-  // useEffect(() => {
-  //   getData();
-  // }, []);
+  useEffect(() => {
+    getData();
+  }, []);
 
-  // const getData = async () => {
-  //   setLoading(true);
-  //   // fetch
-  //   // setData
-  //   setLoading(false);
-  // };
+  const getData = async () => {
+    setLoading(true);
+    // fetch
+    // setData
+    setLoading(false);
+  };
 
   return (
     <div className="main">
-      <div className="main__following" />
-      <div className="main__follow" />
-      <div className="main__timeline" />
-      <div className="main__message" />
+      <div className="main__following"></div>
+      <div className="main__follow"></div>
+      <div className="main__timeline"></div>
+      <div className="main__message"></div>
     </div>
   );
 }
